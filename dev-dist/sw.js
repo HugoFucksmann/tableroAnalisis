89d76609d1e0f46ef80482934c591468dbcf03d3
HugoFucksmann <hugoffuksmann@gmail.com>
2026-04-25 23:16:43 -0300

    deploy

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bfq851o1ahg"
+    "revision": "0.n5c08mmt3bs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
